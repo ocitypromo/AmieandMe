5568c1f8bcd0231e53c7f70c9676065ff295d8f3
April Cook <ocitypromo@gmail.com>
2021-08-06 11:05:32 -0500

    arrays part two woth Amie

--- arraysHomework1.js
+++ arraysHomework1.js
@@ -1,12 +1,37 @@
+//3 things functions do: 
+// 1.) Take in data. 
+// 2.) Do some work on the data.
+// 3.) Returns the result.
+
+//What a for loop would look like if it were a while loop.
+//var index = 0;
+//while (index < array.length)
+//{
+    //Stuff inside the for loop.
+
+    //index++;
+//}
 
 //Takes an array and adds every value in the array together.
 //Returns the result of the addition.
 function addAllArrayValuesTogether(array) {
+    var runningTotal = 0;
 
+    for (let index = 0; index < array.length; index++) {
+        runningTotal = array[index] + runningTotal;
+    }
+
+    return runningTotal;
 }
+
 
 //Homework: inish the addAllArrayValuesTogether function.
 //Test it and print the result.
-
 var testArray1 = [7, 32, 9, 1, 2, 3, 4, 5, 6, 8, 12, 22, 21];
+//var total = (testArray1 + array.length[index]);
+//testArray1 + array[index]
+//addAllArrayValuesTogether(testArray1);
+var result = addAllArrayValuesTogether(testArray1);
+console.log(result);
 
+console.log(addAllArrayValuesTogether(testArray1));
---
+++ arraysHomework2.js
@@ -0,0 +1,14 @@
+
+
+//Takes an array of numbers and either adds or subtracts the numbers together depending on their index.
+//On even indexes you add the the values
+//On odd indexes you subtract the values.
+//This function only returns the result.
+function evenOddAddSubtract(array)  {
+
+}
+
+var testArray1 = [7, 32, 9, 1, 2, 3, 4, 5, 6, 8, 12, 22, 21];
+
+//Call the evenOddAddSubtract function and pass in testArray1
+//Print the result to the console here.
